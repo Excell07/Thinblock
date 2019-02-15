fed9c38f2ff25965f1bd9eb40ea1d60c934ef93e
Mark <developerzero7@gmail.com>
2019-02-15 20:58:59 +0800

    add scroll up feature

--- client/src/components/navigation/Footer.js
+++ client/src/components/navigation/Footer.js
@@ -1,43 +1,87 @@
-import React, { Component } from 'react'
-import logo from '../../images/negative-logo.png'
+import React, { Component } from "react";
+import logo from "../../images/negative-logo.png";
+import arrow from "../../images/chevron.png";
 
+import ScrollToTop from "react-scroll-up";
 
 export default class Footer extends Component {
   render() {
     return (
       <div>
-        <div className="sc-gPEVay footer__outer">
+        <div className="footer__outer">
           <div className="container">
             <div className="justify-content-sm-center row">
               <div lgoffset="2" className="col-lg-6 col-12">
-                <div className="sc-iRbamj footer__outer-logo"><img src={logo} alt="logo" /></div>
-              </div>
-                <div className="col-lg-2 col-4">
-                  <ul className="sc-jlyJG footer__list"><span>Developers</span>
-                    <a href="https://docs.thinblock.io" target="_blank" rel="noopener noreferrer"><li>Documentation</li></a>
-                    <a href="https://github.com/thinblock" target="_blank" rel="noopener noreferrer"><li>Github</li></a>
-                    <a href="/"><li>API Status</li></a>
-                  </ul>
+                <div className="footer__outer-logo">
+                  <img src={logo} alt="logo" />
                 </div>
-                  <div className="col-lg-2 col-4">
-                    <ul className="sc-jlyJG footer__list">
-                      <span>RESOURCES</span>
-                      <a href="/"><li>Conditions</li></a>
-                      <a href="/"><li>Terms & Conditions</li></a>
-                      <a href="/"><li>Privacy Policy</li></a>
-                    </ul>
-                  </div>
-                  <div className="col-lg-2 col-4">
-                    <ul className="sc-jlyJG footer__list">
-                      <span>Support</span>
-                      <a href="https://twitter.com/thinblock" target="_blank" rel="noopener noreferrer"><li>Twitter</li></a>
-                      <a href="mailto:contact@thinblock.io" target="_blank" rel="noopener noreferrer"><li>Email</li></a>
-                    </ul>
-                  </div>
+              </div>
+              <div className="col-lg-2 col-4">
+                <ul className="footer__list">
+                  <span>Developers</span>
+                  <a
+                    href="https://docs.thinblock.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Documentation</li>
+                  </a>
+                  <a
+                    href="https://github.com/thinblock"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Github</li>
+                  </a>
+                  <a href="/">
+                    <li>API Status</li>
+                  </a>
+                </ul>
+              </div>
+              <div className="col-lg-2 col-4">
+                <ul className="footer__list">
+                  <span>RESOURCES</span>
+                  <a href="/">
+                    <li>Conditions</li>
+                  </a>
+                  <a href="/">
+                    <li>Terms & Conditions</li>
+                  </a>
+                  <a href="/">
+                    <li>Privacy Policy</li>
+                  </a>
+                </ul>
+              </div>
+              <div className="col-lg-2 col-4">
+                <ul className="footer__list">
+                  <span>Support</span>
+                  <a
+                    href="https://twitter.com/thinblock"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Twitter</li>
+                  </a>
+                  <a
+                    href="mailto:contact@thinblock.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Email</li>
+                  </a>
+                </ul>
               </div>
             </div>
           </div>
+          <ScrollToTop showUnder={160}>
+            <img
+              src={arrow}
+              alt="up"
+              style={{ width: 30, filter: "opacity(25%)" }}
+            />
+          </ScrollToTop>
+        </div>
       </div>
-    )
+    );
   }
 }
--- client/src/components/navigation/MobileNav.js
+++ client/src/components/navigation/MobileNav.js
@@ -1,67 +1,90 @@
-import React, {Component} from 'react';
-import {NavHashLink as NavLink} from 'react-router-hash-link';
-import brand from '../../images/negative-logo.png';
+import React, { Component } from "react";
+import { NavHashLink as NavLink } from "react-router-hash-link";
+import brand from "../../images/negative-logo.png";
 
 class MobileNav extends Component {
-  render(){
-    // let overlayState;
-    // let mobileSlideout;
-    // let dynamicClass;
-
-    // if (this.props.mobileToggle === true && this.props.mobileView === true){
-    //   overlayState = "overlay--active";
-    //   mobileSlideout = "mobile-nav--slide-out";
-    // } else if (this.props.desktopView === true){
-    //   dynamicClass = "justify-content-center"
-    // } else {
-    //   overlayState = "overlay--hidden";
-    //   mobileSlideout = "";
-    // }
-    
-    // if (dynamicClass === "justify-content-center"){
-    //   dynamicClass = "justify-content-end"
-    // }
-
-    
-
+  render() {
     return (
       <div className="bg-custom">
-        {/* <div className={`overlay ${overlayState}`}></div> */}
-
         <div className="container">
-            <nav className="navbar navbar-expand-md navbar-light bg-transparent">
-              <a className="navbar-brand" href="#home"><img src={brand} alt="" className="nav__list__section__item__img-container__img"/></a>
-            
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <nav className="navbar navbar-expand-md navbar-light bg-transparent">
+            <a className="navbar-brand" href="#home">
+              <img
+                src={brand}
+                alt=""
+                className="nav__list__section__item__img-container__img"
+              />
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarToggler"
+              aria-controls="navbarToggler"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon" />
             </button>
-              <div className="collapse navbar-collapse" id="navbarToggler">
-                <ul className="mr-auto navbar-nav"></ul>
-                <ul className="navbar-nav">
-                {/* <ul className={`nav navbar-nav ${dynamicClass}`}> */}
-                  <li className="nav-item">
-                  <NavLink onClick={this.props.toggleBtn} className="nav-link mobile-nav__list__item" to="#solution">Our Solution</NavLink>
-                  </li>
-                  <li className="nav-item">
-                  <NavLink onClick={this.props.toggleBtn} className="nav-link mobile-nav__list__item" to="#roadmap" activeclassname="mobile-nav-active">RoadMap</NavLink>
-                  </li>
-                  <li className="nav-item">
-                  <NavLink onClick={this.props.toggleBtn} className="nav-link mobile-nav__list__item" to="#" activeclassname="mobile-nav-active">API</NavLink>
-                  </li>
-                  <li className="nav-item">
-                  <NavLink onClick={this.props.toggleBtn} className="nav-link mobile-nav__list__item" to="#" activeclassname="mobile-nav-active">FAQ</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <a onClick={this.props.toggleBtn} href="mailto:contact@thinblock.io" className="nav-link mobile-nav__list__item" activeclassname="mobile-nav-active">Contact Sales</a>
-                  {/* <NavLink onClick={this.props.toggleBtn} className="nav-link mobile-nav__list__item" to="mailto:contact@thinblock.io" activeclassname="mobile-nav-active">Contact Sales</NavLink> */}
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          {/* </ul> */}
+            <div className="collapse navbar-collapse" id="navbarToggler">
+              <ul className="mr-auto navbar-nav" />
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink
+                    onClick={this.props.toggleBtn}
+                    className="nav-link mobile-nav__list__item"
+                    to="#solution"
+                  >
+                    Our Solution
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    onClick={this.props.toggleBtn}
+                    className="nav-link mobile-nav__list__item"
+                    to="#roadmap"
+                    activeclassname="mobile-nav-active"
+                  >
+                    RoadMap
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    onClick={this.props.toggleBtn}
+                    className="nav-link mobile-nav__list__item"
+                    to="#"
+                    activeclassname="mobile-nav-active"
+                  >
+                    API
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    onClick={this.props.toggleBtn}
+                    className="nav-link mobile-nav__list__item"
+                    to="#"
+                    activeclassname="mobile-nav-active"
+                  >
+                    FAQ
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <a
+                    onClick={this.props.toggleBtn}
+                    href="mailto:contact@thinblock.io"
+                    className="nav-link mobile-nav__list__item"
+                    activeclassname="mobile-nav-active"
+                  >
+                    Contact Sales
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
       </div>
-    )
+    );
   }
 }
 
